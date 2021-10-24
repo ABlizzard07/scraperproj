79688ed10b117fb3b6a8f7f86cc5fefd5dafbf51
UniversalOverlord123 <68557624+UniversalOverlord123@users.noreply.github.com>
2021-10-24 15:13:16 -0400

    Add files via upload

--- merge.py
+++ merge.py
@@ -24,6 +24,21 @@ data_1 = brightest_stars[1:]
 headers_2 = brown_dwarfs[0]
 data_2 = brown_dwarfs[1:]
 
+planet_masses = []
+planet_radiuses = []
+planet_names = []
+
+for data in data_1:
+  planet_masses.append(planet_data[3])
+  planet_radiuses.append(planet_data[7])
+  planet_names.append(planet_data[1])
+
+planet_gravity = []
+
+for index, name in enumerate(planet_names):
+  gravity = (float(planet_masses[index])*5.972e+24) / (float(planet_radiuses[index])*float(planet_radiuses[index])*6371000*6371000) * 6.674e-11
+  planet_gravity.append(gravity)
+
 headers = headers_1 + headers_2
 data = []
 for index, data_row in enumerate(data_1):
